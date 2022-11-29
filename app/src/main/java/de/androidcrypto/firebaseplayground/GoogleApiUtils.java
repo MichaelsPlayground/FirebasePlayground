bcac607d7c06195a759f18da4cd667abdb77384f
michaelfehr <michael_fehrgmx.de>
2022-11-29 01:53:34 +0100

    updated

---
+++ app/src/main/java/de/androidcrypto/firebaseplayground/GoogleApiUtils.java
@@ -0,0 +1,42 @@
+package de.androidcrypto.firebaseplayground;
+
+import android.app.Activity;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RestrictTo;
+
+import com.google.android.gms.auth.api.credentials.Credentials;
+import com.google.android.gms.auth.api.credentials.CredentialsClient;
+import com.google.android.gms.auth.api.credentials.CredentialsOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
+/**
+ * taken from FirebaseUi-Android
+ */
+
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+public final class GoogleApiUtils {
+    private GoogleApiUtils() {
+        throw new AssertionError("No instance for you!");
+    }
+
+    public static boolean isPlayServicesAvailable(@NonNull Context context) {
+        return GoogleApiAvailability
+                .getInstance()
+                .isGooglePlayServicesAvailable(context) == ConnectionResult.SUCCESS;
+    }
+
+    @NonNull
+    public static CredentialsClient getCredentialsClient(@NonNull Context context) {
+        CredentialsOptions options = new CredentialsOptions.Builder()
+                .forceEnableSaveDialog()
+                .build();
+        if (context instanceof Activity) {
+            return Credentials.getClient((Activity) context, options);
+        } else {
+            return Credentials.getClient(context, options);
+        }
+    }
+}
